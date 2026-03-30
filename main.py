4261a94b05947641e1707eef936887c27a7f6b6e
Nooshin Borhani rayeni <nooshin.borhani2022@gmail.com>
2026-03-30 12:13:37 +0200

    add faq retrieval system

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
+import json
+
+with open("faq.json", "r") as file:
+    faq_data = json.load(file)
+
+def find_answer(user_input, faq_data):
+    user_input = user_input.lower()
+
+    for item in faq_data:
+        for question in item["questions"]:
+            if user_input in question.lower():
+                return item["answer"]
+
+    return None
+
 print("AI Support Agent Started")
 
 while True:
     user_message = input("You: ")
 
-    if user_message.lower() == "hello":
-        print("Bot: Hi! How can I help you today?")
-    elif user_message.lower() == "refund":
-        print("Bot: I can help with refund requests.")
-    elif user_message.lower() == "bye":
+    if user_message.lower() == "bye":
         print("Bot: Goodbye!")
         break
+
+    answer = find_answer(user_message, faq_data)
+
+    if answer:
+        print("Bot:", answer)
     else:
         print("Bot: Sorry, I do not understand your request yet.")
