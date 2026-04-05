8b6b262e0b9c068099d94bd0351cca0274406229
Nooshin Borhani rayeni <nooshin.borhani2022@gmail.com>
2026-04-05 12:05:15 +0200

    improve contraction handling in preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,25 +1,36 @@
-import re
-
+import string
 
 CONTRACTIONS = {
     "can't": "cannot",
     "won't": "will not",
+    "n't": " not",
+    "i'm": "i am",
+    "it's": "it is",
+    "he's": "he is",
+    "she's": "she is",
+    "that's": "that is",
+    "what's": "what is",
+    "there's": "there is",
+    "i've": "i have",
+    "we've": "we have",
+    "they've": "they have",
+    "i'll": "i will",
+    "you'll": "you will",
+    "they'll": "they will",
+    "isn't": "is not",
+    "aren't": "are not",
+    "wasn't": "was not",
+    "weren't": "were not",
     "don't": "do not",
     "doesn't": "does not",
     "didn't": "did not",
-    "i'm": "i am",
-    "it's": "it is",
-    "you're": "you are",
-    "i've": "i have",
-    "we're": "we are",
-    "they're": "they are",
 }
 
 
 STOPWORDS = {
     "i", "me", "my", "you", "your", "the", "a", "an", "is", "are", "am",
     "to", "for", "of", "and", "or", "in", "on", "at", "it", "this", "that",
-    "be", "can", "cant", "cannot", "do", "did", "was", "were", "with",
+    "be", "can", "cannot", "do", "did", "was", "were", "with",
     "working"
 }
 
@@ -35,7 +46,11 @@ def expand_contractions(text):
 
 def preprocess_text(text):
     text = expand_contractions(text)
-    text = re.sub(r"[^\w\s]", "", text)
+
+    text = text.translate(str.maketrans("", "", string.punctuation))
+
     words = text.split()
+
     words = [w for w in words if w not in STOPWORDS]
+
     return " ".join(words)
